9ead0d606de256d6860962c6dab6c586bfb5ff39
santosh <santosh.ece06@gmail.com>
2018-07-18 15:48:19 +0530

    lazy and loading continue.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {OwlCarousel} from 'ngx-owl-carousel';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('owlElement') owlElement: OwlCarousel;
+
   public message: string;
   public items1: Array<number> = [1, 2, 3, 4, 5];
 
@@ -16,4 +21,25 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.message = 'Hello Home';
   }
+
+  customFun() {
+    console.log('Event handling owl-carousel!!!.');
+  }
+
+  fun1() {
+    console.log('FUN!');
+  }
+
+  funt1(e) {
+    console.log(e);
+  }
+
+  lazyload1(e) {
+    console.log('Lazy INit',  e);
+  }
+
+  lazyload2(e) {
+    console.log('Lazy Init Done: ',  e);
+  }
+
 }
